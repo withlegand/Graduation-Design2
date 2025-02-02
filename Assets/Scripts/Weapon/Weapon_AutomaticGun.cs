4c52cbcbdf3d7b075d6946b519c16119ca97d160
withlegand <16655569018@139.com>
2025-02-03 00:20:35 +0800

    准心代码

--- Assets/Scripts/Weapon/Weapon_AutomaticGun.cs
+++ Assets/Scripts/Weapon/Weapon_AutomaticGun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //武器音效内部类
 [System.Serializable]
@@ -16,10 +17,16 @@ public class SoundClips
 
 public class Weapon_AutomaticGun : Weapon
 {
+    private PlayerController playerController;
+
     [Header("武器部件位置")]
     [Tooltip("射击的位置")]public Transform ShootPoint;//射线打出的位置
     public Transform BulletShootPoint;//子弹特效打出的位置
     [Tooltip("子弹壳抛出的位置")] public Transform CasingBulletSpawnPoint;
+
+    [Header("子弹预制体和特效")]
+    public Transform bulletPrefab;//
+    public Transform casingPrefab;//
 
     [Header("枪械属性")]
     [Tooltip("武器射程")] public float range;
@@ -44,16 +51,30 @@ public class Weapon_AutomaticGun : Weapon
     private AudioSource mainAudioSource;
     public SoundClips SoundClips;
 
+    [Header("UI")]
+    public Image[] crossQuarterImgs;//准心
+    public float currentExpanedDegree;//当前准心的开合度
+    private float crossExpanedDegree;//每帧准心开合度
+    private float maxCrossDegree;//最大开合度
+    public Text ammoTextUI;
+    public Text shootModeTextUI;
+
+    public PlayerController.MovementsState state;
+
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
         mainAudioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
     {
         muzzleFlashLight.enabled = false;
+        crossExpanedDegree = 50f;
+        maxCrossDegree = 300f;
         lightDuration = 0.02f;
         range = 300f;
+        bulletForce = 100f;
         bulletLeft = bulletMag * 5;
         currentBullets = bulletMag;
     }
@@ -62,7 +83,23 @@ public class Weapon_AutomaticGun : Weapon
     
     private void Update()
     {
-        
+        state = playerController.state;//
+        if (state ==PlayerController.MovementsState.walking && Vector3.SqrMagnitude(playerController.moveDirection)>0 && state != PlayerController.MovementsState.runing && state != PlayerController.MovementsState.crouching)
+        {
+            //移动时的准心开合度
+            ExpaningCrossUpdate(crossExpanedDegree);
+        }
+        else if (state != PlayerController.MovementsState.walking && state == PlayerController.MovementsState.runing && state != PlayerController.MovementsState.crouching)
+        {
+            //奔跑时的准心开合度（2倍）
+            ExpaningCrossUpdate(crossExpanedDegree*2);
+        }
+        else
+        {
+            //站立或者下蹲，不调整准心开合度
+            ExpaningCrossUpdate(0);
+        }
+
         if (Input.GetMouseButton(0))
         {
             GunFire();//开枪射击
@@ -86,19 +123,29 @@ public class Weapon_AutomaticGun : Weapon
         StartCoroutine(MuzzleFlashLight());//开火灯光
         muzzlePatic.Emit(1);//发射一个枪口火焰粒子
         sparkPatic.Emit(Random.Range(minSparkEmission,maxSparkEmission));//发射枪口火星粒子
+        StartCoroutine(Shoot_Cross());
 
         RaycastHit hit;
         Vector3 shootDirection = ShootPoint.forward;//向前方射击
         shootDirection = shootDirection + ShootPoint.TransformDirection(new Vector3(Random.Range(-SpreadFactor,SpreadFactor), Random.Range(-SpreadFactor, SpreadFactor)));
         if (Physics.Raycast(ShootPoint.position,shootDirection,out hit,range))
         {
+            Transform bullet;
+
+            bullet =  (Transform)Instantiate(bulletPrefab,BulletShootPoint.transform.position,BulletShootPoint.transform.rotation);
+            //给子弹拖尾一个向前的速度力(加上射线打出去的偏移值）
+            bullet.GetComponent<Rigidbody>().velocity = (bullet.transform.forward + shootDirection) * bulletForce;
             print(hit.transform.gameObject.name);
         }
+        //实例抛弹壳
+        Instantiate(casingPrefab,CasingBulletSpawnPoint.transform.position,CasingBulletSpawnPoint.transform.rotation);
         
         mainAudioSource.clip = SoundClips.shootSound;
-        mainAudioSource.Play();
+        mainAudioSource.Play();//播放设射击音效
         fireTimer = 0;//重置计时器
         currentBullets--;//当前子弹数累减
+
+        
     }
 
     //设置开火的灯光
@@ -121,11 +168,44 @@ public class Weapon_AutomaticGun : Weapon
     {
     }
 
-    public override void ExpaningCrossUpdate(float Degree)
-    {
-    }
-
+    
     public override void Reload()
     {
+    }
+    
+    public override void ExpaningCrossUpdate(float expandDegree)
+    {
+        if (currentExpanedDegree < expandDegree - 5)
+        {
+            ExpendCross(150*Time.deltaTime);
+        }
+        else if (currentExpanedDegree > expandDegree + 5)
+        {
+            ExpendCross(-300 * Time.deltaTime);
+        }
+    }
+
+    //改变准心的开合度 并记录当前准心开合度
+    public void ExpendCross(float add)
+    {
+        crossQuarterImgs[0].transform.localPosition += new Vector3(-add,0,0);//左准心
+        crossQuarterImgs[1].transform.localPosition += new Vector3(add, 0,0);//右准心
+        crossQuarterImgs[2].transform.localPosition += new Vector3(-0,add,0);//上准心
+        crossQuarterImgs[3].transform.localPosition += new Vector3(0,-add,0);//下准心
+
+        currentExpanedDegree += add;//保存当前准心开合度
+        currentExpanedDegree = Mathf.Clamp(currentExpanedDegree, 0, maxCrossDegree);//限制准心开合度大小
+
+    }
+
+    //携程 调用准心开合度 1帧执行5次
+    //只负责射击时瞬间增大准心
+    public IEnumerator Shoot_Cross()
+    {
+        yield return null;
+        for (int i = 0; i<5;i++)
+        {
+            ExpendCross(Time.deltaTime * 500);
+        }
     }
 }
